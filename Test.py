23e1a792d64fe5bd1f563dce7ae02b2e744b0d80
Ganesh-Malladi <61865736+Ganesh-Malladi@users.noreply.github.com>
2021-11-22 21:45:34 +0530

    Update Test.py

--- Test.py
+++ Test.py
@@ -5,13 +5,30 @@ import unittest
 from SE_jenkins import add_6_to_list
 
 class Testadd6(unittest.TestCase):
-    def test_list_int(self):
+    def test1_list_int(self):
         """
-        Test case to add two numbers
+        Test case to add 6 to elements of list
         """
         data = [1,2,3,4,5]
         result = add_6_to_list(data)
         self.assertEqual(result, [7,8,9,10,11])
+    
+    def test2_list_int(self):
+        """
+        Test case to add 6 to elements of list
+        """
+        data = [1,9,21,-3,11]
+        result = add_6_to_list(data)
+        self.assertEqual(result, [7,15,27,3,17])
+        
+    def test3_list_int(self):
+        """
+        Test case to add 6 to elements of list
+        """
+        data = [93,69,44,11,74]
+        result = add_6_to_list(data)
+        self.assertEqual(result, [99,75,50,17,80])
+
 
 if __name__ == '__main__':
     unittest.main()
