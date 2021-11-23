d40e2aa438c49806eb4c2ce0f07a75340c5f8d15
Ganesh-Malladi <61865736+Ganesh-Malladi@users.noreply.github.com>
2021-11-23 11:39:59 +0530

    Update Test.py

--- Test.py
+++ Test.py
@@ -11,7 +11,7 @@ class Testadd6(unittest.TestCase):
         """
         data = [1,2,3,4,5]
         result = add_6_to_list(data)
-        self.assertEqual(result, [7,8,9,10,11])
+        self.assertEqual(result, [1,2,3,4,5])
 
 if __name__ == '__main__':
     unittest.main()
